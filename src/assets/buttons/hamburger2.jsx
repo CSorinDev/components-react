78be4df4eed04e77cd99853986363f758d322d6c
Sorin <csorin.dev@gmail.com>
2024-07-20 01:59:53 +0000

    hamburger 2 done

--- src/assets/buttons/hamburger2.jsx
+++ src/assets/buttons/hamburger2.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react"
 
-export default function Hamburger1() {
+export default function Hamburger2() {
     const [hamburgerSwitch, setHamburgerSwitch ] = useState(false)
 
     return(
         <article className="p-4 w-fit text-white border shadow-sm shadow-white">
-            <h1>Hamburger 1</h1>
+            <h1>Hamburger 2</h1>
             <button 
                 onClick={() => setHamburgerSwitch(!hamburgerSwitch)}
                 className={`
@@ -14,19 +14,19 @@ export default function Hamburger1() {
                 <div
                     className={`
                         transition-all w-[35px] h-[5px] bg-white rounded-full absolute
-                        ${hamburgerSwitch ? 'top-[15px] rotate-45' : 'top-[5px] rotate-0'}
+                        ${hamburgerSwitch ? 'top-[15px] rotate-[calc(45deg+180deg)]' : 'top-[5px] rotate-0'}
                         `}
                 ></div>
                 <div
                     className={`
-                        transition-all h-[5px] bg-white rounded-full absolute top-[15px] mx-auto origin-center
-                        ${hamburgerSwitch ? 'w-[0]' : 'w-[35px]' }
+                        transition-all w-[35px] h-[5px] bg-white rounded-full absolute top-[15px] mx-auto origin-center
+                        ${hamburgerSwitch ? 'opacity-0' : 'opacity-1' }
                         `}
                 ></div>
                 <div
                     className={`
                         transition-all w-[35px] h-[5px] bg-white rounded-full absolute 
-                        ${hamburgerSwitch ? 'top-[15px] -rotate-45' : 'top-[25px] rotate-0'}
+                        ${hamburgerSwitch ? 'top-[15px] -rotate-[calc(45deg+180deg)]' : 'top-[25px] rotate-0'}
                         `}
                 ></div>
             </button>
--- src/routes/buttons.jsx
+++ src/routes/buttons.jsx
@@ -1,10 +1,14 @@
 import Hamburger1 from "../assets/buttons/hamburger1";
+import Hamburger2 from "../assets/buttons/hamburger2";
 import Layout from "../layouts/layout";
 
 export default function Buttons() {
     return (
         <Layout>
-            <Hamburger1 />
+            <section className="flex gap-4">
+                <Hamburger1 />
+                <Hamburger2 />
+            </section>
         </Layout>
     )
 }
