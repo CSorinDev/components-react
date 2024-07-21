0457f0fec6832a419a49e7f5bd62e67713f7b41e
Sorin <csorin.dev@gmail.com>
2024-07-21 22:13:38 +0000

    copied message aligned with copy-icon

--- src/assets/getCode.jsx
+++ src/assets/getCode.jsx
@@ -6,7 +6,7 @@ export default function GetCode({codeToCopy}) {
       try {
         await navigator.clipboard.writeText(codeToCopy);
         setIsCopied(true);
-        setTimeout(() => { setIsCopied(false); }, 1000);
+        setTimeout(() => { setIsCopied(false); }, 500);
       } catch (err) {
         console.error("Failed to copy code:", err);
       }
@@ -14,16 +14,16 @@ export default function GetCode({codeToCopy}) {
   
     return (
       <>
-        <button onClick={handleCopyClick} aria-label="Copy Code">
+        <button className="relative" onClick={handleCopyClick} aria-label="Copy Code">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-copy"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
+          <span className={
+              isCopied 
+              ? "absolute origin-bottom-left bottom-1/2 left-1/2 z-10 w-max h-max transition-all duration-75 text-white bg-green-500 rounded-lg py-2 px-4" 
+              : "absolute origin-bottom-left bottom-1/2 left-1/2 z-10 w-max h-max transition-all duration-75 text-white bg-green-500 rounded-lg py-2 px-4 scale-0"
+          }>
+              Copied to clipboard
+          </span>
         </button>
-        <span className={
-            isCopied 
-            ? "absolute transition-all text-white bg-green-500 rounded-lg py-2 px-4 bottom-2 right-2" 
-            : "absolute transition-all text-white bg-green-500 rounded-lg py-2 px-4 bottom-2 right-2 scale-0"
-        }>
-            Copied to clipboard
-        </span>
       </>
     );
   }
