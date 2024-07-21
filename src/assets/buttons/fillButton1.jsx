33f29103d47cac3a93fcbe3aee6e23660c75e49c
Sorin <csorin.dev@gmail.com>
2024-07-21 02:42:57 +0000

    ok

--- src/assets/buttons/fillButton1.jsx
+++ src/assets/buttons/fillButton1.jsx
@@ -1,8 +1,10 @@
 import Article from "../article";
 
 export default function FillButton1() {
-    const code = `
-<button
+    return (
+        <Article 
+            title="Fill button 1"
+            code={`<button
     className="border-2 py-2 px-6 relative rounded-lg
     after:bg-blue-500 after:content-[''] after:absolute after:py-2 after:top-0 after:left-0
     after:overflow-hidden after:origin-left after:mx-auto after:w-full after:scale-0
@@ -11,16 +13,11 @@ export default function FillButton1() {
         <span className="z-10 relative">
             Button
         </span>
-</button>
-`
-    console.log(code)
-
-    return (
-        <Article code={code} title="Fill button 1">
+</button>`}>
             <button
                 className="border-2 py-2 px-6 relative rounded-lg
                 after:bg-blue-500 after:content-[''] after:absolute after:py-2 after:top-0 after:left-0
-                ºafter:overflow-hidden after:origin-left after:mx-auto after:w-full after:scale-0
+                after:overflow-hidden after:origin-left after:mx-auto after:w-full after:scale-0
                 hover:after:scale-100 hover:after:px-6 after:transition-all after:h-full after:rounded-lg
                 ">
                     <span className="z-10 relative">
