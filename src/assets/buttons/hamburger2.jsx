27380ae4f87e9b7cddd132f3d97ffc60f01b7dae
Soare <csorin.dev@gmail.com>
2024-08-06 09:13:02 +0200

    till hb3

--- src/assets/buttons/hamburger2.jsx
+++ src/assets/buttons/hamburger2.jsx
@@ -7,31 +7,39 @@ export default function Hamburger2() {
     return(
         <Article
             title='Hamburger 2'
-            code={`<button 
-    onClick={() => setHamburgerSwitch(!hamburgerSwitch)}
-    className='size-[35px] relative'>
-    <div
-        className={
-            hamburgerSwitch ?
-            'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] rotate-[calc(45deg+180deg)]' :
-            'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[5px] rotate-0'
-        }>
-    </div>
-    <div
-        className={
-            hamburgerSwitch ?
-            'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] mx-auto origin-center opacity-0' :
-            'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] mx-auto origin-center'
-        }>
-    </div>
-    <div
-        className={
-            hamburgerSwitch ?
-            'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] -rotate-[calc(45deg+180deg)]' :
-            'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[25px] rotate-0'
-        }>
-    </div>
-</button>`}>
+            code={`import { useState } from "react"
+
+export default function HamburgerButton() {
+    const [hamburgerSwitch, setHamburgerSwitch ] = useState(false)
+
+    return(
+        <button 
+            onClick={() => setHamburgerSwitch(!hamburgerSwitch)}
+            className='size-[35px] relative'>
+            <div
+                className={
+                    hamburgerSwitch ?
+                    'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] rotate-[calc(45deg+180deg)]' :
+                    'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[5px] rotate-0'
+                }>
+            </div>
+            <div
+                className={
+                    hamburgerSwitch ?
+                    'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] mx-auto origin-center opacity-0' :
+                    'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] mx-auto origin-center'
+                }>
+            </div>
+            <div
+                className={
+                    hamburgerSwitch ?
+                    'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] -rotate-[calc(45deg+180deg)]' :
+                    'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[25px] rotate-0'
+                }>
+            </div>
+        </button>
+    )
+}`}>
             <button 
                 onClick={() => setHamburgerSwitch(!hamburgerSwitch)}
                 className='size-[35px] relative'>
--- src/assets/buttons/hamburger3.jsx
+++ src/assets/buttons/hamburger3.jsx
@@ -7,31 +7,39 @@ export default function Hamburger3() {
     return(
         <Article
             title='Hamburger 3'
-            code={`<button 
-    onClick={() => setHamburgerSwitch(!hamburgerSwitch)}
-    className='size-[35px] relative'>
-    <div
-        className={
-            hamburgerSwitch
-            ? 'transition-all w-[35px] h-[5px] rounded-full absolute top-[15px] rotate-[calc(45deg+180deg)] bg-red-500'
-            : 'transition-all w-[35px] h-[5px] rounded-full absolute top-[5px] rotate-0 bg-black'
-        }>
-    </div>
-    <div
-        className={
-            hamburgerSwitch
-            ? 'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] mx-auto origin-center opacity-0'
-            : 'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] mx-auto origin-center'
-        }>
-    </div>
-    <div
-        className={
-            hamburgerSwitch
-            ? 'transition-all w-[35px] h-[5px] rounded-full absolute top-[15px] -rotate-[calc(45deg+180deg)] bg-red-500'
-            : 'transition-all w-[35px] h-[5px] rounded-full absolute top-[25px] rotate-0 bg-black'
-        }>
-    </div>
-</button>`}>
+            code={`import { useState } from "react"
+
+export default function HamburgerButton() {
+    const [hamburgerSwitch, setHamburgerSwitch ] = useState(false)
+
+    return(
+        <button 
+            onClick={() => setHamburgerSwitch(!hamburgerSwitch)}
+            className='size-[35px] relative'>
+            <div
+                className={
+                    hamburgerSwitch
+                    ? 'transition-all w-[35px] h-[5px] rounded-full absolute top-[15px] rotate-[calc(45deg+180deg)] bg-red-500'
+                    : 'transition-all w-[35px] h-[5px] rounded-full absolute top-[5px] rotate-0 bg-black'
+                }>
+            </div>
+            <div
+                className={
+                    hamburgerSwitch
+                    ? 'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] mx-auto origin-center opacity-0'
+                    : 'transition-all w-[35px] h-[5px] bg-black rounded-full absolute top-[15px] mx-auto origin-center'
+                }>
+            </div>
+            <div
+                className={
+                    hamburgerSwitch
+                    ? 'transition-all w-[35px] h-[5px] rounded-full absolute top-[15px] -rotate-[calc(45deg+180deg)] bg-red-500'
+                    : 'transition-all w-[35px] h-[5px] rounded-full absolute top-[25px] rotate-0 bg-black'
+                }>
+            </div>
+        </button>
+    )
+}`}>
             <button 
                 onClick={() => setHamburgerSwitch(!hamburgerSwitch)}
                 className='size-[35px] relative'>
